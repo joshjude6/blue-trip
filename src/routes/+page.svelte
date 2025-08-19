528edd3f4b4f41c9969bd555320defaa2af3d338
Josh Jude <joshjude6@gmail.com>
2025-08-19 13:09:27 +0200

    test exclamation

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -17,7 +17,7 @@ leaderboard
     <section class="max-w-4xl mx-auto px-4">
         <div class="bg-white rounded-lg p-6 sm:p-8">
             <p class="font-kalmansk text-black text-base sm:text-lg leading-relaxed text-center">
-                Blod, svette, og KI har gitt oss denne nettsiden for blåturen som vi SNART skal på! 
+                Blod, svette, og KI har gitt oss denne nettsiden for blåturen som vi SNART skal på!!
                 På denne nettsiden kan dere melde kryss på hverandre, registrere antall enheter, og gjette hvor vi skal. 
                 Jeg kom ikke på mer features, men gjerne foreslå noe, så vibe-koder jeg det raskt (jeg betalte for claude.ai i én måned via jobb selv om det funka helt jævlig for VHDL😎).
             </p>
